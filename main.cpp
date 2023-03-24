481be65f73354887e11ea8e8e8e9ed8ccbbc1103
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2023-03-24 15:05:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main() {
 
-cin >> a >> b >> c;
+    int a, b, c;
+    cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cout << (a * b) / 2 << endl;
+    cout << a * a << endl;
+    cout << a * b << endl;
+    cout << (a + b) * c / 2 << endl;
 
-
+    return 0;
 }
